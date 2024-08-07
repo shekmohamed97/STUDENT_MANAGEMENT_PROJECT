e401eaed60cb90be901ebccb500d87af47851695
shekmohamed97 <shek37576@gamail.com>
2024-08-07 13:18:58 +0530

    Cors Update 9

--- CorsSec/CorsSeting.js
+++ CorsSec/CorsSeting.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 app.use(cors({
     origin: [
-               "http://localhost:5173",
+               "http://localhost:5173",//
                "https://elegant-paletas-e1b8b5.netlify.app",
                "https://student-management-project.onrender.com"
              ],////////////
